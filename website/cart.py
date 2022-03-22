c8db4fedfa80842bd76a8bc0ccc730ff29bd615a
joshua0828 <jccamposmcgregor@my.waketech.edu>
2022-03-22 16:33:00 -0400

    added the coded that was missing in cart.py to show total price.

--- website/cart.py
+++ website/cart.py
@@ -15,8 +15,19 @@ stripe.api_key = 'sk_test_51KOEoTEAaICJ0GdRPRiVmPSZIQQ9DVtzWqeNtuevHa01p74QcR5wC
 
 @cart.route('/website-cart', methods=['GET', 'POST'])
 def website_cart():
+  tip = request.form.get('tipp')
+  
+  total = 0
+  subtotal = total_price_items()
+
+  if tip == '' or tip == None:
+    tip = 0
+    total = total_price_items()
+  else: 
+    total = total_price_items() + float(tip)
   rows = Cart.query.filter(Cart.id).count()
-  return render_template('cart.html', user=current_user, item=get_cart_items(), rows=rows)
+  return render_template('cart.html', user=current_user, item=get_cart_items(), rows=rows, total='{:,.2f}'.format(total), subtotal='{:,.2f}'.format(subtotal),
+   tip='{:,.2f}'.format(float(tip)))
 
 
 @cart.route('/delete/<int:id>')
@@ -88,3 +99,18 @@ def get_cart_items():
     names.append(cart.name)
     test_cart_items.append(grabber)
   return test_cart_items
+
+def total_price_items():
+
+  ids = [id[0] for id in Cart.query.with_entities(Cart.id).all()]
+  total = 0
+  prices = []
+  for id in ids:
+    item = Cart.query.filter_by(id=id).first()
+    prices.append(item.price)
+    
+  for price in prices:
+    total = total + price
+  return total
+
+  
